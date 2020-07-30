bf8a02691275dd45e0358e48afa48d09b2ca2364
Stephen Lang <stephenlang91@gmail.com>
2020-07-30 13:07:19 -0400

    updated media root

--- blog/settings.py
+++ blog/settings.py
@@ -139,8 +139,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_in_env')
 ]
 VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(VENV_PATH, 'staticfiles')
-MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 
 #Tinymce
